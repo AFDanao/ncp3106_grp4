a426ef597919a1a496e7312642d8d8f839e1466b
masamonte <samonte.michaelangelo@ue.edu.ph>
2023-12-06 15:04:20 +0800

    index
    done na

--- index.php
+++ index.php
@@ -2,15 +2,12 @@
 $error = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get the entered username and password from the form
     $enteredUsername = $_POST["username"];
     $enteredPassword = $_POST["password"];
 
-    // Define the default username and password
     $defaultUsername = "admin";
     $defaultPassword = "admin";
 
-    // Check if the entered username and password match the default values
     if ($enteredUsername === $defaultUsername && $enteredPassword === $defaultPassword) {
         header('Location: select.php');
     } else {
@@ -31,22 +28,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         body {
             font-family: 'Poppins', sans-serif;
             height: 100vh;
-            background-image: linear-gradient(#f7dad9, #fb7404);
             margin: 0;
-        }
-
-        .login-container {
             display: flex;
             align-items: center;
             justify-content: center;
+        }
+
+        #engineering-image {
+            background: url('scpes.png') center center no-repeat;
+            background-size: cover;
+            width: 100vw;
             height: 100vh;
+            position: fixed;
+            z-index: -1; 
         }
 
         .container {
-            width: 400px; 
+            width: 350px;
+            height: auto;
             padding: 16px;
             background-color: white;
-            border-radius: 10px; 
+            border-radius: 10px;
+            z-index: 1; 
         }
 
         label {
@@ -55,7 +58,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         input[type=text],
         input[type=password] {
-            margin-bottom: 10px;
+            margin-bottom: 20px;
         }
 
         button {
@@ -70,7 +73,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         .password-eye {
             position: absolute;
             right: 1px;
-            top: 55%;
+            top: 50%;
             transform: translateY(-50%);
             cursor: pointer;
         }
@@ -79,57 +82,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             background-color: transparent;
             border: none;
         }
-
-        .img-container {
-            width: 1000px; 
-            max-width: 100%;
-            margin-right: auto;
-        }
     </style>
 </head>
 <body>
-    <div class="container-fluid login-container">
-        <div class="row">
-            <div class="col-md-6">
-                <div class="img-container">
-                    <img src="engineering.png" alt="Engineering Image" class="img-fluid">
+    <div id="engineering-image"></div>
+
+    <div class="container">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" placeholder="Enter your username" name="username" required>
+            </div>
+
+            <div class="form-group">
+                <label for="password">Password</label>
+                <div class="input-group">
+                    <input type="password" class="form-control" placeholder="Enter your password" name="password" id="password" required>
+                    <div class="input-group-append">
+                        <span class="input-group-text password-eye" onclick="togglePasswordVisibility()">
+                            <i id="eye-icon" class="fa fa-eye"></i>
+                        </span>
+                    </div>
                 </div>
             </div>
 
-            <div class="col-md-6">
-                <div class="container">
-                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                        <div class="form-group">
-                            <label for="username">Username</label>
-                            <input type="text" class="form-control" placeholder="Enter your username" name="username" required>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password">Password</label>
-                            <div class="input-group">
-                                <input type="password" class="form-control" placeholder="Enter your password" name="password" id="password" required>
-                                <div class="input-group-append">
-                                    <span class="input-group-text password-eye" onclick="togglePasswordVisibility()">
-                                        <i id="eye-icon" class="fa fa-eye"></i>
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-check">
-                            <input type="checkbox" class="form-check-input" name="remember">
-                            <label class="form-check-label small-text" for="remember">Remember Me</label>
-                        </div>
-
-                        <div class="form-group mt-3">
-                            <button type="submit" class="btn btn-dark btn-block">Login</button>
-                        </div>
-                        
-                        <span><?php echo $error ?></span>
-                    </form>
-                </div>
+            <div class="form-check mt-5">
+                <input type="checkbox" class="form-check-input" name="remember">
+                <label class="form-check-label small-text" for="remember">Remember Me</label>
             </div>
-        </div>
+
+            <div class="form-group mt-2">
+                <button type="submit" class="btn btn-dark btn-block">Login</button>
+            </div>
+            
+            <span><?php echo $error ?></span>
+        </form>
     </div>
 
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
